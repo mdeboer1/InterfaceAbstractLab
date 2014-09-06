2440cef073ce6c15da6af06be77d0a33db23c072
markr_000 <markr_000@Jet>
2014-09-06 17:13:35 -0500

    Added Startup and ProgrammingCourse interface to be superclass, added methods to superclass.

---
+++ src/lab2/ProgrammingCourse.java
@@ -0,0 +1,23 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package lab2;
+
+/**
+ *
+ * @author mdeboer1
+ */
+public interface ProgrammingCourse {
+    
+    public String getCourseNumber();
+    
+    public void setCourseNumber(String courseNumber);
+    
+    public void setCourseName(String courseName);
+    
+    public String getCourseName();
+
+}
